e9ee420f68b734e455eeb6a2c40b0f49f3cef4a9
Erik Anderson <efa@eecs.berkeley.edu>
2020-05-20 22:18:52 -0700

    added some generic make functionality into make tool

--- make/make/__init__.py
+++ make/make/__init__.py
@@ -29,12 +29,29 @@ class MakeJob:
     dependencies: List[str]
     actions: List[str]
 
+@dataclass
+class MakeTarget:
+    name: str
+    outputs: List[str]
+    description: str
+    spacing: str
+    dependencies: List[str]
+    actions: List[str]
+
 
 class Make(JinjaTool):
     """Make buildfile tool"""
     def __init__(self, db: Database, log: Callable[[str, LogLevel], None]):
         super(Make, self).__init__(db, log)
+        self.make = self.get_db(self.get_namespace("Make"))
+        # Get jobs! and targets
+        jobs = self.get_db("jobs")
+        self.max_num_chars = max([len(j) for j in jobs.keys()])
+        self.max_num_chars = max([len(t["name"]) for t in self.make["targets"]] + 
+            [self.max_num_chars]) 
         self.jobs = self.make_jobs()
+        self.targets = self.make_targets()
+
 
     def steps(self) -> List[Callable[[], None]]:
         """Returns a list of functions to run for each step"""
@@ -42,24 +59,41 @@ class Make(JinjaTool):
 
     def make_jobs(self) -> List[MakeJob]:
         """Returns list of jobs"""
-        # determine maximum length of name (for spacing)
         jobs = self.get_db("jobs")
-        max_num_chars = max([len(j) for j in jobs.keys()])
-        # create job list
         job_list = []
         for k, v in jobs.items():
             try:
                 descr = v["description"]
             except KeyError:
                 descr = f'No description for job "{k}"'
-            spacing = (max_num_chars - len(k)) * " "
+            spacing = (self.max_num_chars - len(k)) * " "
             job_list.append(
                 MakeJob(name=k,
                         description=descr,
                         dependencies=[],
                         spacing=spacing + 4 * " ",
                         actions=[self.get_command(k)]))
+
         return job_list
+    
+    def make_targets(self) -> List[MakeJob]:
+        """Returns list of jobs"""
+        target_list = []
+        # Create job list from configuration generators
+        for t in self.make["targets"]:
+            descr = t["description"] if t["description"] else f'Target "{t["name"]}"'
+            dependencies = t["dependencies"] if t["dependencies"] else [] 
+            outputs = t["outputs"] if t["outputs"] else [] 
+            spacing = (self.max_num_chars - len(t["name"])) * " "
+            target_list.append(
+                MakeTarget(name=t["name"],
+                        description=descr,
+                        outputs=outputs,
+                        dependencies=dependencies,
+                        spacing=spacing + 4 * " ",
+                        actions=t["commands"]))
+
+        return target_list 
 
     def render_tasks(self):
         """Generates tasks file that should be imported in the Makefile
@@ -67,7 +101,7 @@ class Make(JinjaTool):
         """
         output = os.path.join(self.get_db('internal.work_dir'),
                               'Makefile.toolbox')
-        self.render_to_file("Makefile.toolbox", output, jobs=self.jobs)
+        self.render_to_file("Makefile.toolbox", output, jobs=self.jobs, targets=self.targets)
 
     def render_makefile(self):
         """Looks to see if Makefile exists otherwise populates with new one"""
